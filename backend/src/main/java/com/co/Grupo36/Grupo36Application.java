f5dbdfd25c8a4c3b1004e1961f180e183087388b
eocando <eocando@bancolombia.com.co>
2021-10-06 20:49:56 -0500

    Proyecto Spring boot v0.1

---
+++ backend/src/main/java/com/co/Grupo36/Grupo36Application.java
@@ -0,0 +1,13 @@
+package com.co.Grupo36;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo36Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/co/Grupo36/Grupo36ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.Grupo36;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo36ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
